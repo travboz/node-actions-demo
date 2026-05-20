1ad34aa7c2df16bacb62ef8a5c7b972f2bc55479
Travis Bozic <travis.bozic@hotmail.com>
2026-05-20 13:43:50 +1000

    feat(workflow): add login workflow to use secrets properly

---
+++ .github/scripts/login_avery.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -X POST https://dummyjson.com/auth/login \
+    -H 'Content-Type: application/json' \
+    -d '{"username": "$API_USERNAME","password": "$API_PASSWORD"}' \
+    | jq -r .accessToken
